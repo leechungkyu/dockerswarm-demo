7e668c15cfdaf1a79087d8d9ecf731449d252ba1
leechungkyu <ck1214.lee@gmail.com>
2020-03-04 14:48:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 var os = require('os');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(`<h1>I'm ${os.hostname()} v2v2!</h1>`);
+    res.end(`<h1>I'm ${os.hostname()} v1!</h1>`);
 }).listen(443);
